3974f1959229b4d3c0558a90fac8d797d3c2073d
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-11-26 16:01:37 +0200

    added base entity schema

---
+++ LearnPortal.DAL/Entities/Course/Skill.cs
@@ -0,0 +1,15 @@
+﻿using LearnPortal.DAL.Interfaces;
+
+namespace LearnPortal.DAL.Entities.Course
+{
+    public class Skill : ISkill
+    {
+        public Guid Id { get; set; }
+        
+        public string Title { get; set; }
+        
+        public string Description { get; set; }
+        
+        public Guid OwnerId { get; set; }
+    }
+}
---
+++ LearnPortal.DAL/Entities/User/User.cs
@@ -0,0 +1,19 @@
+﻿using LearnPortal.DAL.Interfaces;
+
+namespace LearnPortal.DAL.Entities.User
+{
+    public class User : IUser
+    {
+        public Guid Id { get; set; }
+        
+        public string UserName { get; set; }
+        
+        public string Email { get; set; }
+        
+        public IEnumerable<ICourse> FinishedCourses { get; set; }
+        
+        public IEnumerable<ICourse> InProgressCourses { get; set; }
+        
+        public IEnumerable<ISkill> Skills { get; set; }
+    }
+}
