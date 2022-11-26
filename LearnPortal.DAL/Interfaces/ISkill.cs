97d6e73a1c33afd54d048a232318daaf25fd61e2
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-11-26 15:49:26 +0200

    added interfaces for materials, courses, repo, skills,user

---
+++ LearnPortal.DAL/Interfaces/ISkill.cs
@@ -0,0 +1,13 @@
+﻿namespace LearnPortal.DAL.Interfaces
+{
+    public interface ISkill
+    {
+        Guid Id { get; set; }
+
+        string Title { get; set; }
+
+        string Description { get; set; }
+
+        Guid OwnerId { get; set; }
+    }
+}
---
+++ LearnPortal.DAL/Interfaces/IUser.cs
@@ -0,0 +1,17 @@
+﻿namespace LearnPortal.DAL.Interfaces
+{
+    public interface IUser
+    {
+        Guid Id { get; set; }
+
+        string UserName { get; set; }
+
+        string Email { get; set; }
+
+        IEnumerable<ICourse> FinishedCourses { get; set; }
+
+        IEnumerable<ICourse> InProgressCourses { get; set; }
+
+        IEnumerable<ISkill> Skills { get; set; }
+    }
+}
