2311bf2baceacbc979cf0764526bc955a2fe1ccf
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-11-27 14:12:18 +0200

    added book service and interface for it

---
+++ LearnPortal.BLL/Interfaces/IBookService.cs
@@ -0,0 +1,19 @@
+﻿using LearnPortal.BLL.DTO;
+using LearnPortal.DAL.Entities.Course;
+using LearnPortal.DAL.Entities.Material;
+
+namespace LearnPortal.BLL.Interfaces
+{
+    public interface IBookService
+    {
+        Task CreateBook(BookDTO book);
+
+        Task DeleteBook(int id);
+
+        Task UpdateBook(BookDTO course);
+
+        IEnumerable<BookDTO> FindBook(Func<Book, bool> predicate);
+
+        Task<BookDTO> GetBook(int id);
+    }
+}
