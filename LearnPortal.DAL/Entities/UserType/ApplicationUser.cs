97ef47c9992a175af58ef60e85356369459810b6
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-12-16 20:11:23 +0200

    added application user model for identity

---
+++ LearnPortal.DAL/Entities/UserType/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace LearnPortal.DAL.Entities.UserType
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual User User { get; set; }
+    }
+}
