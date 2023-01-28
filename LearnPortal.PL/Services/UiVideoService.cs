fcdaa64d852678b698d18b6f674d3f991bcffb23
Kirill_divine <kirill.sokolov96@gmail.com>
2023-01-28 19:05:23 +0200

    add UiVideoService, add method to show video, add video

---
+++ LearnPortal.PL/Services/UiVideoService.cs
@@ -0,0 +1,90 @@
+﻿using LearnPortal.BLL.DTO;
+using LearnPortal.BLL.Services;
+using LearnPortal.DAL.Entities.UserType;
+using LearnPortal.PL.ViewModels;
+
+namespace LearnPortal.PL.Services
+{
+    public class UiVideoService
+    {
+        private readonly VideoService _videoService;
+
+        public UserDTO CurrentUser { get; set; }
+
+        public UiVideoService(UserDTO currentUser)
+        {
+            _videoService = new VideoService(currentUser);
+            CurrentUser = currentUser;
+        }
+
+        public VideoViewModel EnteringVideoFields()
+        {
+            Console.WriteLine("Enter a Title of a video:");
+            string title = Console.ReadLine();
+            PrinterService.Message("Enter a Description of a book:");
+            string description = Console.ReadLine();
+            Console.WriteLine("Enter a resolution of a video (480,720,1080, etc):");
+            int resolution = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a duration of a video in minutes:");
+            int duration = int.Parse(Console.ReadLine());
+
+            return new VideoViewModel
+            {
+                Title = title,
+                Description = description,
+                Duration = duration,
+                Resolution = resolution,
+            };
+        }
+
+        public async Task CreateVideoAsync()
+        {
+            try
+            {
+                VideoViewModel video = EnteringVideoFields();
+
+                PrinterService.BreakLine();
+                if (video != null)
+                {
+                    await _videoService.CreateVideo(new VideoDTO
+                    {
+                        Title = video.Title,
+                        Resolution = video.Resolution,
+                        Description = video.Description,
+                        Duration = video.Duration,
+                    });
+                }
+                else
+                {
+                    PrinterService.ErrorMsg("Try again!");
+                }
+            }
+            catch (Exception ex)
+            {
+                PrinterService.ErrorMsg(ex.Message);
+            }
+        }
+
+        public async Task ShowVideoAsync()
+        {
+            var id = UserInputService.GetId();
+            if (id!= Guid.Empty)
+            {
+                var video = await _videoService.GetVideo(id);
+                PrinterService.Print(new VideoViewModel
+                {
+                    Id = video.Id,
+                    OwnerId = video.OwnerId,
+                    Title = video.Title,
+                    Description = video.Description,
+                    Duration = video.Duration,
+                    Resolution = video.Resolution,
+                });
+            }
+            else
+            {
+                PrinterService.ErrorMsg("Incorrect Id");
+            }
+        }
+    }
+}
