57e198f1323cc6b73498c6561db104d56a391641
Kirill_divine <kirill.sokolov96@gmail.com>
2023-02-24 14:57:32 +0200

    IUiService.cs interface is complete

--- LearnPortal.PL/Interfaces/IUiService.cs
+++ LearnPortal.PL/Interfaces/IUiService.cs
@@ -1,6 +1,21 @@
-﻿namespace LearnPortal.PL.Interfaces
+﻿using LearnPortal.BLL.DTO;
+
+namespace LearnPortal.PL.Interfaces
 {
-    internal interface IUiService
+    internal interface IUiService<T>
     {
+        UserDTO CurrentUser { get; }
+
+        T EnteringFields();
+
+        Task CreateAsync();
+
+        Task GetAsync();
+
+        Task UpdateAsync();
+
+        Task DeleteAsync();
+
+        Task GetAllAsync();
     }
 }
