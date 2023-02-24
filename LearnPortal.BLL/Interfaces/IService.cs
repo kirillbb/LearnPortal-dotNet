50444c9efcb63000d78d56167a56ff7ef79ad344
Kirill_divine <kirill.sokolov96@gmail.com>
2023-02-24 15:15:37 +0200

    IService.cs on BLL interface is complete

---
+++ LearnPortal.BLL/Interfaces/IService.cs
@@ -0,0 +1,19 @@
+﻿using LearnPortal.BLL.DTO;
+
+namespace LearnPortal.BLL.Interfaces
+{
+    internal interface IService<T>
+    {
+        Task CreateAsync(T item);
+
+        Task DeleteAsync(Guid id);
+
+        Task UpdateAsync(T item);
+
+        IEnumerable<VideoDTO> Find(Func<T, bool> predicate);
+
+        Task<VideoDTO> GetAsync(Guid id);
+
+        Task<IEnumerable<T>> GetAsync();
+    }
+}
